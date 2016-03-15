81c550aa60a39dac0c501a61dee21bf58338a946
CarlosNoriega <charlie02.06.93@gmail.com>
2016-03-15 17:31:52 -0600

    Update leer.py

--- leer.py
+++ leer.py
@@ -1,25 +1,25 @@
 import json
 import urllib
 #import urllib.request
-
+ 
 
 
 url='http://10.43.14.124:3000/api'
-r = urllib.request.urlopen(url)
+r = urllib.urlopen(url)
 
 data =json.loads('{"_id":"56e84d461a12721263023879","name":"Area 1","generalCapacity":20,"handicapCapacity":5,"generalAvailable":20,"handicapAvailable":5,"__v":0}')
 contador=0
 print(data['name'])
 palabra= str(r.read())
 
-for y in range(3, palabra.__len__()):
+for y in range(0, palabra.__len__()):
         if(palabra[y]=="{"):
             contador=contador+1;
 
 arreglo = ['']*contador
 contador=-1
 escribe=False
-for x in range(3, palabra.__len__()):
+for x in range(0, palabra.__len__()):
     if (palabra[x]=="{"):
         contador=contador+1
         escribe=True
@@ -36,4 +36,6 @@ pal=arreglo[0]
 ls=json.loads(pal)
 
 print (ls['name'])
-print (data)
+pal=arreglo[1]
+ls=json.loads(pal)
+print (ls['name'])
