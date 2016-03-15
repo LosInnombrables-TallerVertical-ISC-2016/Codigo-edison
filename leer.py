05ab7234035e23895e3e19b32eb9312b93b8eea1
CarlosNoriega <charlie02.06.93@gmail.com>
2016-03-15 16:29:11 -0600

    q
    q

---
+++ leer.py
@@ -0,0 +1,38 @@
+import json
+import urllib.request
+from pip._vendor.requests.packages.urllib3.connectionpool import xrange
+from setuptools.command.build_ext import if_dl
+from builtins import int
+url='http://10.43.14.124:3000/api'
+r = urllib.request.urlopen(url)
+
+data =json.loads('{"_id":"56e84d461a12721263023879","name":"Area 1","generalCapacity":20,"handicapCapacity":5,"generalAvailable":20,"handicapAvailable":5,"__v":0}')
+contador=0
+print(data['name'])
+palabra= str(r.read())
+
+for y in xrange(3, palabra.__len__()):
+        if(palabra[y]=="{"):
+            contador=contador+1;
+
+arreglo = ['']*contador
+contador=-1
+escribe=False
+for x in xrange(3, palabra.__len__()):
+    if (palabra[x]=="{"):
+        contador=contador+1
+        escribe=True
+    
+    if (escribe):
+        arreglo[contador]=arreglo[contador]+palabra[x]
+    
+    if (palabra[x]=="}"):
+        escribe=False        
+
+print(arreglo[0])
+print(arreglo[1])
+pal=arreglo[0]
+ls=json.loads(pal)
+
+print (ls['name'])
+print (data)
