247373c50e72468a5ae53c9c374405c74a64e2f9
root <chechare_94@hotmail.com>
2016-03-16 18:35:28 +0000

    Controlador de Edison

--- new.py
+++ new.py
@@ -5,103 +5,96 @@ import mraa
 import pyupm_i2clcd as lcd
 import sys
 import random
-import os
 
 import json
 import urllib
-#import urllib.request
-url='http://10.43.14.124:3000/api'
-r = urllib.urlopen(url)
+import subprocess
 
-data =json.loads('{"_id":"56e84d461a12721263023879","name":"Area 1","generalCapacity":20,"handicapCapacity":5,"generalAvailable":20,"handicapAvailable":5,"__v":0}')
-contador=0
-print(data['name'])
-palabra= str(r.read())
+#Area que administra
+areaName = "Area 1"
 
-for y in range(0, palabra.__len__()):
-        if(palabra[y]=="{"):
-            contador=contador+1;
+#Direccion del servidor
+server='http://10.43.14.124:3000/api'
 
-arreglo = ['']*contador
-contador=-1
-escribe=False
-for x in range(0, palabra.__len__()):
-    if (palabra[x]=="{"):
-        contador=contador+1
-        escribe=True
-    
-    if (escribe):
-        arreglo[contador]=arreglo[contador]+palabra[x]
-    
-    if (palabra[x]=="}"):
-        escribe=False        
+r = urllib.urlopen(server)
 
-print(arreglo[0])
-print(arreglo[1])
-pal=arreglo[0]
-ls=json.loads(pal)
+#Leyendo query a server.
+jsonStr = r.read()
 
-print (ls['name'])
-pal=arreglo[1]
-ls=json.loads(pal)
-print (ls['name'])
+#Convirtiendo en un objeto JSON
+jsonResult = json.loads(jsonStr);
 
+#Numero de areas
+size = len(jsonResult)
 
+#Buscar objeto del area a administrar
+for i in (0, size - 1):
+    print(jsonResult[i])
+    if(jsonResult[i]["name"] == areaName):
+        area = jsonResult[i]
 
+#Espacios ocupados
+occupied = area['generalCapacity'] - area['generalAvailable']
+#Capacidad del area
+capacity = area['generalCapacity']
+overload = 0
 
-
-
-pal=arreglo[0]
-ls=json.loads(pal)
-counter = ls['generalAvailable']
-pal=arreglo[1]
-ls=json.loads(pal)
-counter2 = ls['generalAvailable']
-# Create the button object using GPIO pin 0
+# Create the button object using GPIO pin 2
 button = mraa.Gpio(2)
+# Create the buzzer object using GPIO pin 3
 button2 = mraa.Gpio(3)
-# Create the buzzer object using GPIO pin 5
 
 button.dir(mraa.DIR_IN)
 button2.dir(mraa.DIR_IN)
 
+#Inicializar led
 ledPin = mraa.Gpio(5)
 ledPin.dir(mraa.DIR_OUT)
 ledPin.write(1)
 
-
-#Crete the lcd object 
+#Crete the lcd object
 lcdDisplay = lcd.Jhd1313m1(0, 0x3E, 0x62)
 lcdDisplay.setColor(0,0,0)
 entra=False
 sale=False
 lcdDisplay.setCursor(0, 0)
-lcdDisplay.write("Hello world!!")
+lcdDisplay.write(str(area["name"]))
 lcdDisplay.setCursor(1, 7)
-lcdDisplay.write(str(counter2))
-r = random.randint(0, 255)
-g = random.randint(0, 255)
-b = random.randint(0, 255)
+lcdDisplay.write(str(occupied) + "/"+ str(capacity))
+
+#Color blanco para la pantalla
+if(occupied == capacity):
+    r = 204
+    g = 6
+    b = 5
+else:
+    r = 205
+    g = 205
+    b = 205
 lcdDisplay.setColor(r,g,b)
+
 # Read the input and print, waiting one second between readings
 while 1:
-    
-    
-        
     if(entra):
         if(button2.read()!=0):
-            os.system('curl -X PUT -H "Cache-Control: no-cache" -H "Content-Type: application/x-www-form-urlencoded" -d "name=Area 1&generalAvailable=1" "http://localhost:3000/api/"')
             lcdDisplay.setCursor(1, 0)
             lcdDisplay.write("                ")
             lcdDisplay.setCursor(1, 0)
-            counter = counter + 1
-            lcdDisplay.write(str(counter))
+
+            if(occupied + 1 <= capacity):
+                occupied = occupied + 1
+                subprocess.check_call(['curl', '-X', 'PUT', '-H', "Cache-Control: no-cache",
+                    '-H', "Content-Type: application/x-www-form-urlencoded", '-d', "name=Area 1&generalAvailable=-1", server])
+            else:
+                overload = overload + 1
+
+            lcdDisplay.write(str(occupied))
             lcdDisplay.setCursor(1, 7)
-            lcdDisplay.write(str(counter2))
-            r = random.randint(0, 255)
-            g = random.randint(0, 255)
-            b = random.randint(0, 255)
-            lcdDisplay.setColor(r,g,b)
+            lcdDisplay.write(str(capacity))
+
+            lcdDisplay.setCursor(1, 9)
+            lcdDisplay.write(str(overload))
+
             print("Boton 2 presionado, entro")
             ledPin.write(0)
             entra=False
@@ -109,27 +102,36 @@ while 1:
             time.sleep(0.5)
             lcdDisplay.setCursor(0, 0)
             lcdDisplay.write("                ")
-    else: 
+            #subprocess.check_call('curl -X PUT -H "Cache-Control: no-cache" -H "Content-Type: application/x-www-form-urlencoded" -d "name=Area 1&generalAvailable=-1" '+server)
+    else:
         if(button2.read() != 0):
             sale=True
             ledPin.write(1)
             print("Boton 2 presionado")
             time.sleep(0.5)
-            
-        
+
+
     if(sale):
         if(button.read()!=0):
             lcdDisplay.setCursor(1, 0)
             lcdDisplay.write("                ")
             lcdDisplay.setCursor(1, 0)
-            counter = counter - 1
-            lcdDisplay.write(str(counter))
+
+            if(occupied - 1 >= 0):
+                if(overload == 0):
+                    occupied = occupied - 1
+                    subprocess.check_call(['curl', '-X', 'PUT', '-H', "Cache-Control: no-cache",
+                        '-H', "Content-Type: application/x-www-form-urlencoded", '-d', "name=Area 1&generalAvailable=1", server])
+                else:
+                    overload = overload - 1
+
+            lcdDisplay.write(str(occupied))
             lcdDisplay.setCursor(1, 7)
-            lcdDisplay.write(str(counter2))
-            r = random.randint(0, 255)
-            g = random.randint(0, 255)
-            b = random.randint(0, 255)
-            lcdDisplay.setColor(r,g,b)
+            lcdDisplay.write(str(capacity))
+
+            lcdDisplay.setCursor(1, 9)
+            lcdDisplay.write(str(overload))
+
             print("Boton 1 presionado, salio")
             ledPin.write(0)
             entra=False
@@ -142,21 +144,25 @@ while 1:
             print("Boton 1 presionado")
             lcdDisplay.setCursor(0, 0)
             lcdDisplay.write("                ")
-            if(counter<counter2):
+            if(occupied<capacity ):
                 lcdDisplay.setCursor(0, 0)
                 lcdDisplay.write("Est-1")
             else:
                 lcdDisplay.setCursor(0, 0)
-                lcdDisplay.write("Est-2")  
-            r = random.randint(0, 255)
-            g = random.randint(0, 255)
-            b = random.randint(0, 255)
-            lcdDisplay.setColor(r,g,b)
+                lcdDisplay.write("Est-2")
+
             time.sleep(0.5)
-    
 
-# Delete the buzzer object
+    if(occupied == capacity):
+        r = 204
+        g = 6
+        b = 5
+    else:
+        r = 205
+        g = 205
+        b = 205
 
+    lcdDisplay.setColor(r,g,b)
 
 # Delete the button object
 del button
